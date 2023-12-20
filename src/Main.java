22502841a5f0393591dd083421551ae791e3a6b3
İmranov <semtexpert@gmail.com>
2023-12-20 16:19:28 +0300

    vücut kitle indeksi hazır

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double boy, kg,vki;
+        Scanner ind= new Scanner(System.in);
+        System.out.print("Lütfen Boyunuzu (metre cinsinde) giriniz: ");
+        boy= ind.nextDouble();
+        Scanner ind2= new Scanner(System.in);
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        kg = ind2.nextDouble();
+
+
+        vki= (kg/(boy*boy));
+        System.out.println("Vücut kütle indeksiniz: " + vki);
+
+
     }
 }
